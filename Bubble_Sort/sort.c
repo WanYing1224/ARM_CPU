57ca2a598fac0ed1ddef2fd640a229c9ee1d1a5e
Kevelyn Lin <kevelynlin1224@gmail.com>
2026-03-12 18:04:42 -0700

    Uplaod bubble sort c code and assembly code.

---
+++ Bubble_Sort/sort.c
@@ -0,0 +1,13 @@
+int main() {
+	int array[10];
+	int i, j, swap;
+	for (i = 0 ; i < 10; i++) {
+		for (j = i+1 ; j < 10 ; j++) {
+			if (array[j] < array[i]) {
+				swap = array[j];
+				array[j] = array[i];
+				array[i] = swap;
+			}
+		}
+	}
+}
---
+++ Bubble_Sort/sort.s
@@ -0,0 +1,93 @@
+	.cpu arm7tdmi
+	.arch armv4t
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"sort.c"
+	.text
+	.align	2
+	.global	main
+	.syntax unified
+	.arm
+	.type	main, %function
+main:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 56
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	sub	sp, sp, #60
+	mov	r3, #0
+	str	r3, [fp, #-8]
+	b	.L2
+.L6:
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	str	r3, [fp, #-12]
+	b	.L3
+.L5:
+	ldr	r3, [fp, #-12]
+	lsl	r3, r3, #2
+	sub	r3, r3, #4
+	add	r3, r3, fp
+	ldr	r2, [r3, #-52]
+	ldr	r3, [fp, #-8]
+	lsl	r3, r3, #2
+	sub	r3, r3, #4
+	add	r3, r3, fp
+	ldr	r3, [r3, #-52]
+	cmp	r2, r3
+	bge	.L4
+	ldr	r3, [fp, #-12]
+	lsl	r3, r3, #2
+	sub	r3, r3, #4
+	add	r3, r3, fp
+	ldr	r3, [r3, #-52]
+	str	r3, [fp, #-16]
+	ldr	r3, [fp, #-8]
+	lsl	r3, r3, #2
+	sub	r3, r3, #4
+	add	r3, r3, fp
+	ldr	r2, [r3, #-52]
+	ldr	r3, [fp, #-12]
+	lsl	r3, r3, #2
+	sub	r3, r3, #4
+	add	r3, r3, fp
+	str	r2, [r3, #-52]
+	ldr	r3, [fp, #-8]
+	lsl	r3, r3, #2
+	sub	r3, r3, #4
+	add	r3, r3, fp
+	ldr	r2, [fp, #-16]
+	str	r2, [r3, #-52]
+.L4:
+	ldr	r3, [fp, #-12]
+	add	r3, r3, #1
+	str	r3, [fp, #-12]
+.L3:
+	ldr	r3, [fp, #-12]
+	cmp	r3, #9
+	ble	.L5
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	str	r3, [fp, #-8]
+.L2:
+	ldr	r3, [fp, #-8]
+	cmp	r3, #9
+	ble	.L6
+	mov	r3, #0
+	mov	r0, r3
+	add	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
+	.size	main, .-main
+	.ident	"GCC: (Arm GNU Toolchain 14.2.Rel1 (Build arm-14.52)) 14.2.1 20241119"
